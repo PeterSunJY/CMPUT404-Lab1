73956273ef430578c4481ea3e6755ddb99a0c70a
PeterSunJY <mrsunjiayuan@gmail.com>
2021-09-07 00:18:48 -0600

    find source code

--- lab1.py
+++ lab1.py
@@ -1,6 +1,12 @@
 import requests
-print("The version of requests library is: " + requests.__version__)
+print("The version of requests library is: " + requests.__version__ + "\n")
 
+print("Here is the Google homepage: ")
 r = requests.get("http://www.google.com")
 print(r)
-print(r.text)
+print(r.text + "\n")
+
+print("Here is the source code of my python script: ")
+code = requests.get("https://raw.githubusercontent.com/PeterSunJY/CMPUT404-Lab1/master/lab1.py")
+code = code.text
+print(code)
